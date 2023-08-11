65adfff9a70cad61c350edaa72638f1277e3ce0a
kshamaakumar <kkumar@seattleu.edu>
2023-08-10 22:32:34 -0700

    Optimized solution

--- Easy/two_sum.py
+++ Easy/two_sum.py
@@ -1,6 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if nums[i]+nums[j] == target:
+        #             return [i,j]
+
+        encountered = {}
+
         for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if nums[i]+nums[j] == target:
-                    return [i,j]
+            diff = target - nums[i]
+            if diff in encountered:
+                return [encountered[diff],i]
+            else:
+                encountered[nums[i]] = i
